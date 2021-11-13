a2bcb00f877eb4b9b8040ad528cad9b191976580
a-waterson <aidanwaterson@gmail.com>
2021-11-13 00:10:20 -0800

    fixed weird linux specific header issue

--- server/include/dbstuff.h
+++ server/include/dbstuff.h
@@ -3,13 +3,9 @@
 #include <dc_posix/dc_fcntl.h>
 #include <dc_posix/dc_ndbm.h>
 
-#include <ndbm.h>
+void db_store(const struct dc_error *err, const struct dc_posix_env *env,
+              const char *key_str, const char *val_str);
 
-void db_store(const struct dc_error *err,
-              const struct dc_posix_env *env, const char *key_str,
-              const char *val_str);
-
-void db_fetch(const struct dc_error *err,
-              const struct dc_posix_env *env, const char *key_str,
-              const char *val_str);
+void db_fetch(const struct dc_error *err, const struct dc_posix_env *env,
+              const char *key_str, const char *val_str);
 #endif
--- server/src/http_request.c
+++ server/src/http_request.c
@@ -1,7 +1,7 @@
 #include "http_.h"
 struct http_request req;
 struct request_line req_line;
-void                process_request(char *request, struct http_request *req)
+void process_request(char *request, struct http_request *req)
 {
     req->req_line.HTTP_VER = "1.1";
 };

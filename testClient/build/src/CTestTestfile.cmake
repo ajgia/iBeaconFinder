5aa709ee351174f947b7d65832fe5495b68cf14f
Aidan-Manjaro <aidanwaterson@gmail.com>
2021-11-18 01:48:36 -0800

    it builds!

---
+++ testClient/build/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/aidanw/work/COMP3980/iBeaconFinder/testClient/src
+# Build directory: /home/aidanw/work/COMP3980/iBeaconFinder/testClient/build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ testClient/include/dbstuff.h
@@ -0,0 +1,11 @@
+#ifndef TEMPLATE_DBSTUFF_H
+#define TEMPLATE_DBSTUFF_H
+#include <dc_posix/dc_fcntl.h>
+#include <dc_posix/dc_ndbm.h>
+
+void db_store(const struct dc_error *err, const struct dc_posix_env *env,
+              const char *key_str, const char *val_str);
+
+void db_fetch(const struct dc_error *err, const struct dc_posix_env *env,
+              const char *key_str, const char *val_str);
+#endif  // TEMPLATE_DBSTUFF_H
